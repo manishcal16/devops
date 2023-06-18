72aeec6664aab6d23ca268cb97618dd236d0d5c0
manishcal16 <manishcal16@gmail.com>
2023-06-18 14:16:31 +0000

    Add project files.

---
+++ shopping/shopping.clienct/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shopping.clienct.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
